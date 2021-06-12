c652aec5a7df8e6fa4b962b338e543f631cb6318
Jivin Mazumder <jivinmazumder11@outlook.com>
2021-06-12 16:57:55 -0400

    v1.0.2

--- npm/run-script.js
+++ npm/run-script.js
@@ -1,6 +1,6 @@
 #!/usr/bin/none
 
 module.exports = () => {
-  console.log("run npx llogs -f file.txt to run a file!")
+  console.log("run npx llogs -f file.txt to watch a file!")
   process.exitCode = 0
 }
